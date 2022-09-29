190cbcad45ea156390cf881066d07fa2dac52021
OmarGomaaFCi <omargomaafcitu@gmail.com>
2022-09-29 02:16:16 +0200

    POST Request Commit

--- app/src/main/java/com/example/sampleretrofit/ApiInterface.kt
+++ app/src/main/java/com/example/sampleretrofit/ApiInterface.kt
@@ -1,16 +1,20 @@
 package com.example.sampleretrofit
 
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Path
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface ApiInterface {
 
+    // GET by Resource
     @GET("posts/{id}")
     fun getModel(@Path("id") id : Int) : Call<Model>
 
+    // GET by Query
     @GET("posts")
     fun getAllPosts(@Query("id") postId : String) : Call<List<Model>>
     // TODO : put "?id" after "posts" --> posts?id=$postId
+
+
+    @POST("posts")
+    fun postModel(@Body body : HashMap<Any,Any>) : Call<Model>
 }
--- app/src/main/java/com/example/sampleretrofit/MainActivity.kt
+++ app/src/main/java/com/example/sampleretrofit/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.sampleretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Display
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import retrofit2.Call
 import retrofit2.Callback
@@ -11,6 +14,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var tvTitle: TextView
+    private lateinit var etTitle: EditText
+    private lateinit var etBody: EditText
+    private lateinit var etUserId: EditText
+    private lateinit var btnPost: Button
+    private lateinit var tvOutPut: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +26,11 @@ class MainActivity : AppCompatActivity() {
 
 
         tvTitle = findViewById(R.id.tvTitle)
+        etTitle = findViewById(R.id.etTitle)
+        etBody = findViewById(R.id.etBody)
+        etUserId = findViewById(R.id.etUserId)
+        btnPost = findViewById(R.id.btnPost)
+        tvOutPut = findViewById(R.id.tvPostOutPut)
 
 
         val retrofitBuilder = Retrofit.Builder()
@@ -26,15 +39,15 @@ class MainActivity : AppCompatActivity() {
             .build()
 
 
-        val apiInterface : ApiInterface = retrofitBuilder.create(ApiInterface::class.java)
+        val apiInterface: ApiInterface = retrofitBuilder.create(ApiInterface::class.java)
 
 
-        val call : Call<Model> = apiInterface.getModel(4)
+        val call: Call<Model> = apiInterface.getModel(4)
 
-        val allCalls : Call<List<Model>> = apiInterface.getAllPosts("2")
+        val allCalls: Call<List<Model>> = apiInterface.getAllPosts("2")
 
 
-        allCalls.enqueue(object : Callback<List<Model>>{
+        allCalls.enqueue(object : Callback<List<Model>> {
             override fun onResponse(call: Call<List<Model>>, response: Response<List<Model>>) {
                 tvTitle.text = response.body()?.get(0)?.title ?: "Not yet"
             }
@@ -43,6 +56,31 @@ class MainActivity : AppCompatActivity() {
                 tvTitle.text = t.message
             }
         })
+
+
+        btnPost.setOnClickListener {
+            val postCall : Call<Model> = apiInterface.postModel(hashMapOf(
+                "title" to etTitle.text.toString(),
+                "body" to etBody.text.toString(),
+                "userId" to etUserId.text.toString().toInt()
+            ))
+
+            postCall.enqueue(object : Callback<Model>{
+                override fun onResponse(call: Call<Model>, response: Response<Model>) {
+                    val result = "ID : ${response.body()?.id} \n" +
+                            "Title : ${response.body()?.title} \n" +
+                            "Body : ${response.body()?.body} \n" +
+                            "User ID : ${response.body()?.userId}"
+                    tvOutPut.text = result
+                }
+
+                override fun onFailure(call: Call<Model>, t: Throwable) {
+                   tvOutPut.text = t.message
+                }
+            })
+
+
+        }
 
     }
 }
